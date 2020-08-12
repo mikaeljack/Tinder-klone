3794e2d7d40c0262fdd6c7cdb713c605fab33733
Mikael Ås Johannessen <mikael.a.johannessen@hiof.no>
2020-08-12 12:36:15 +0200

    Updates

---
+++ precache-manifest.f566b79767bad9c0e4b0b43cff25ded4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f028feaa05c34b6b12e489418afb963b",
+    "url": "/mikaeljack/Tinder-klone.git/index.html"
+  },
+  {
+    "revision": "486c2cb19a1b7ace3749",
+    "url": "/mikaeljack/Tinder-klone.git/static/css/main.0e530403.chunk.css"
+  },
+  {
+    "revision": "888c3b963670ef307949",
+    "url": "/mikaeljack/Tinder-klone.git/static/js/2.cb568db7.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/mikaeljack/Tinder-klone.git/static/js/2.cb568db7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "486c2cb19a1b7ace3749",
+    "url": "/mikaeljack/Tinder-klone.git/static/js/main.016b248a.chunk.js"
+  },
+  {
+    "revision": "f0e1a6b8c1fd5870a18c",
+    "url": "/mikaeljack/Tinder-klone.git/static/js/runtime-main.4cb11332.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/mikaeljack/Tinder-klone.git/precache-manifest.f566b79767bad9c0e4b0b43cff25ded4.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mikaeljack/Tinder-klone.git/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
